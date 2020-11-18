058e7fc1fc285c78d2a6322c8dd143bc5d9767cc
David Rafe <david.rafe@gmail.com>
2020-11-17 20:03:39 -0500

    commit tree 52e3deb04ff36db675d54ba679d30f108c54c0fe

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+# def prime?(int)
+#     if array_int = (int..int).to_a
+#     array_int.any? do |num|
+#          int % num
+#     end
+#     else array_int = (int..int - 1).to_a
+#         array_int.any? do |num|
+#              int % num == 0
+#         end
+#     end
+# end
+require "pry"
+
+def prime?(int)    
+    if int <= 1
+        return false
+    elsif int == 2
+        return true
+    else
+    array_int = (2..int-1).to_a
+    !array_int.any? do |num|
+       int % num == 0
+    end
+    end
+end
+
